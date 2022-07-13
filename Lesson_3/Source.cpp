073233a90454ec9ff5529e96e51c2d949f427011
VoS <a@a.ru>
2022-07-13 20:06:15 +0400

    Les3

--- Lesson_3/Source.cpp
+++ Lesson_3/Source.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <iomanip>
+#include <limits>
 
-int main()
+#include <string>
+
+int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "");
 
+	const unsigned text_w = 15;
+	const unsigned digit_w = 20;
 
+	std::cout << std::endl << std::setw(text_w) << "short";
+	std::cout << std::setw(text_w) << "size is " << sizeof(short) << " byte(s)";
 
 
 
